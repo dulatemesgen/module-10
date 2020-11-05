385a44099422e3c9d42ce5c63f0fd504ad264690
Dula <temesgen.dula@gmail.com>
2020-11-05 07:36:16 -0600

    commit 2

--- class_definitions/employee.py
+++ class_definitions/employee.py
@@ -28,6 +28,17 @@ class Employee:
             return self.first_name + " " + self.last_name + "\n" \
                    + self.address + "\n" + self.phone_number + "\n" \
                    + "Hourly employee: $" + str(self.salary) + "/hour" + "\n" + str(self.start_date)
+    def __str__(self):
+        return 'Employee: First Name =' + str(self.first_name) + ', Last Name=' \
+        + str(self.last_name) + ', Address=' + str(self.address) + ', Phone=' \
+        + str(self.phone_number) + ', Salary=$' + str(self.salary) \
+        + ', Start Date=' + str(self.start_date) + ', Salaried=' + str(self.salaried)
+
+    def __repr__(self):
+        return 'Employee: First Name =' + repr(self.first_name) + ', Last Name=' \
+               + repr(self.last_name) + ', Address=' + str(self.address) + ', Phone=' \
+               + repr(self.phone_number) + ', Salary=$' + repr(self.salary) \
+               + ', Start Date=' + repr(self.start_date) + ', Salaried=' + repr(self.salaried)
 
 tdate = date.today()
 empl = Employee("Temesgen", "Dula", "5912 park ave., Des Moines IA 50213", "515-555-4000", True, tdate, 35000)
@@ -36,5 +47,5 @@ print(empl)
 
 empl2 = Employee("Temesgen", "Dula", "5912 park ave., Des Moines IA 50213", "515-555-4000", False, tdate, 7.25)
 print(empl2.display())
-print(empl2)
+print(repr(empl2))
 
